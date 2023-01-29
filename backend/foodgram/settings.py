195326ceab0819d684e080a5f9bb63e1adfed993
Dmitry <d.gorelov22@gmail.com>
2023-01-29 14:43:48 +0300

    Исправил пагинацию

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -138,7 +138,7 @@ AUTH_USER_MODEL = 'users.User'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+        'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
