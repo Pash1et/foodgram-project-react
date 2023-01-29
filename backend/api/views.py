97fb8d2e234ad483e3105c80e190619f1f150ef3
Dmitry <d.gorelov22@gmail.com>
2023-01-29 15:00:30 +0300

    Исправил доступы

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import get_object_or_404
 
 from rest_framework import status, views, viewsets
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from recipes.models import (Favorite, Ingredient, IngredientAmount, Recipe,
