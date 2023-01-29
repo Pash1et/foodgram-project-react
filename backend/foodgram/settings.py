0a34db0e7a9da03cbf50d92601cc9379ff8787e6
Dmitry <d.gorelov22@gmail.com>
2023-01-29 14:35:50 +0300

    Исправил пагинацию

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -143,6 +143,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     ),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 6,
 }
