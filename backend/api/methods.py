36c1af40c6fcaebc6d49d05085ef3f9931e8bc91
Dmitry <d.gorelov22@gmail.com>
2023-02-01 01:11:32 +0300

    ревью 2

--- backend/api/methods.py
+++ backend/api/methods.py
@@ -8,11 +8,7 @@ from recipes.models import Recipe
 
 def custom_post(self, request, pk, serializer):
     user = request.user
-    serializer = serializer(
-            data={
-                'recipe_id': pk, 'user_id': user.id
-            }
-        )
+    serializer = serializer(data={'recipe_id': pk, 'user_id': user.id})
     serializer.is_valid(raise_exception=True)
     serializer.save(user=user)
     return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -20,8 +16,6 @@ def custom_post(self, request, pk, serializer):
 def custom_delete(self, request, pk, model):
     user = request.user
     recipe = get_object_or_404(Recipe, pk=pk)
-    model = get_object_or_404(
-            model, user=user, recipe=recipe
-        )
+    model = get_object_or_404(model, user=user, recipe=recipe)
     model.delete()
     return Response(status=status.HTTP_204_NO_CONTENT)
