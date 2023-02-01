7e1885a5987824aede1109cdc4a16b1b73b10301
Dmitry <d.gorelov22@gmail.com>
2023-02-01 14:23:09 +0300

    исправлена информация о пользователях

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,18 +1,34 @@
 from django.shortcuts import get_object_or_404
 from drf_extra_fields.fields import Base64ImageField
 from rest_framework import serializers
+from djoser.serializers import UserSerializer
 
 from recipes.models import (Favorite, Ingredient, IngredientAmount, Recipe,
                             ShoppingCart, Tag, TagRecipe)
 from users.models import Follow, User
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(UserSerializer):
+    """Cериализатор djoser для управления пользователем"""
+    is_subscribed = serializers.SerializerMethodField()
 
     class Meta:
         model = User
-        fields = ('email', 'id', 'username',
-                  'first_name', 'last_name',)
+        fields = (
+            'email',
+            'id',
+            'username',
+            'first_name',
+            'last_name',
+            'is_subscribed',
+        )
+
+    def get_is_subscribed(self, obj):
+        """ Определение подписки пользователя на автора """
+        user = self.context.get('request').user
+        if user.is_anonymous or obj.username == user:
+            return False
+        return Follow.objects.filter(user=user, author=obj).exists()
 
 
 class TagSerializer(serializers.ModelSerializer):
--- backend/api/views.py
+++ backend/api/views.py
@@ -5,6 +5,7 @@ from rest_framework import status, views, viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from djoser.views import UserViewSet
 
 from api.methods import custom_delete, custom_post
 from recipes.models import Favorite, Ingredient, Recipe, ShoppingCart, Tag
@@ -19,7 +20,7 @@ from .serializers import (FavoriteCreateSerializer, FavoriteSerializer,
                           UserSerializer)
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserViewSet(UserViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
     permission_classes = (IsAuthenticated,)
